e6661cdedcaa9b4f88d301c3f1631ba62e8457d6
Pete Warden <pete@petewarden.com>
2011-08-08 21:43:29 -0700

    Fix for phantom node problem, caused by the host name being changed during an Elastic IP assignment

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source :gemcutter
 
 gem "sinatra"
+gem "perftools.rb"
 gem "rack-perftools_profiler"
 
--- perftools.rb
+++ perftools.rb
@@ -1,5 +1,6 @@
 require 'rubygems' if RUBY_VERSION < '1.9'
 require 'sinatra'
+require 'perftools'
 require 'rack/perftools_profiler'
 
 configure :profiling do
@@ -7,6 +8,10 @@ configure :profiling do
 end
 
 get '/' do
+
+  PerfTools::CpuProfiler.start("/tmp/add_numbers_profile") do
+    5_000_000.times{ 1+2+3+4+5 }
+  end
 
   "Hello world!"
   
